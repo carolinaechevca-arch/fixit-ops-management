9060d673ee5d71585c1728e6d0b22781631571be
Caroline-echev <coraline.e1994@gmail.com>
2026-03-10 20:31:06 -0500

    carpetas arquitectura hexagonal

---
+++ src/main/java/com/fixit_ops_management/FixitOpsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.fixit_ops_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixitOpsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixitOpsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fixit_ops_management/FixitOpsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixit_ops_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixitOpsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
